fa614c836ee687f789c8c82712d9e5cd88c0ff14
Camilla <camillamariasi18@gmail.com>
2024-11-30 17:10:52 -0300

    feat: conversão de latitude e longitude.

--- application/functions.py
+++ application/functions.py
@@ -1,4 +1,5 @@
 import random
+import geohash2
 
 def getGeolocationData(lat, long): #recebe latitude e longitude do ActivityMQ
     listGeolocation = [lat, long]
@@ -29,5 +30,25 @@ def inputUserGeoHash():
 
     return geo_hash
 
+def convertToGeoHash(data_GeolocalitionData, precision):
+    geohash = geohash2.encode(data_GeolocalitionData[0], data_GeolocalitionData[1], precision)
+    print(f"Geohash: {geohash}")
+
+    return geohash
+
+
+
+#def lifeControl(geo_hash):
+
+def checkResponseUser(answer_key_geohash, geohash_user_response):
+    if(answer_key_geohash == geohash_user_response):
+        print('------------------------\nParabéns, você acertou!\n------------------------\nProxima rodada')
+        return True
+    else:
+        print("Não foi dessa vez")
+        return False
+
+    
+    
 
 
--- application/main.py
+++ application/main.py
@@ -2,19 +2,20 @@ import pandas as pb
 import socket as sk
 import functions
 
+
 data_Wonders = pb.read_csv('wonders_of_world.csv', encoding='ISO-8859-1')
 data_Tips = pb.read_csv('tips_wonders_of_world.csv', encoding='ISO-8859-1')
 
 #server = sk.
 
 game_Status = True
-
-
+livesPlayer = 3
 
 while game_Status:
+    
 
     data_User = functions.registerUser("Gabriel")
-    data_GeolocalitionData = functions.getGeolocationData("-21.04805556", "-43.21055556")
+    data_GeolocalitionData = functions.getGeolocationData(-8.0476, -34.877)
 
     data_filter = functions.filter_Values_Column(data_Tips)
 
@@ -23,10 +24,15 @@ while game_Status:
 
 
     geo_hash = functions.inputUserGeoHash()
+    answer_key_geohash = functions.convertToGeoHash(data_GeolocalitionData, 7)
+    result_check = functions.checkResponseUser(answer_key_geohash, geo_hash)
 
+    
 
+                               
     menu_op = input("Deseja continuar? [S] para sim ou [N] para não")
     if(menu_op == "N"):
+        print('------------------------\nJogo encerrado!\n------------------------\n')
         game_Status = False
     
 
